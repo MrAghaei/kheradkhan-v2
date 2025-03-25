78984b26d4be8e653714f79000ddb47cc9bd08ee
Mohammad Aghaei <65438728+MrAghaei@users.noreply.github.com>
2025-03-25 11:56:02 +0330

    [#3-IMPLEMENT] add small feature section (#3)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,26 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import localFont from "next/font/local";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
+//
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const iranSans = localFont({
+  src: [
+    {
+      path: "../../public/fonts/IranSans.woff2",
+      weight: "400",
+      style: "normal",
+    },
+  ],
+  display: "swap",
 });
 
 export const metadata: Metadata = {
@@ -24,9 +35,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`bg-white ${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`bg-white ${iranSans.className} antialiased`}>
         {children}
       </body>
     </html>
